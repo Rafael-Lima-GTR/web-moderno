2e3a060d225d9270fdcd04dd92b70ddc489a2a6b
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-12-22 06:14:33 -0300

    Criado o arquivo do Desafio Área da Circunferência

---
+++ exercicios-web/fundamentos/inicio/desafio_area.js
@@ -0,0 +1,13 @@
+// PI = 3.141592
+// PI * raio * raio
+
+const PI = 3.141592;
+let raio = 10;
+let areaDaCircunferencia = PI * (raio * raio);
+
+console.log('Um círculo de Raio igual à ' + raio
+    + ' tem a Área da Circunferência igual à ' + areaDaCircunferencia + 'm2.');
+
+areaDaCircunferencia = Math.PI * (raio * raio);
+console.log('Um círculo de Raio igual à ' + raio
+    + ' tem a Área da Circunferência igual à ' + areaDaCircunferencia + 'm2.');
