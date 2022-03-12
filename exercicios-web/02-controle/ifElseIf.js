01ce3908ae5a6058786ddcf89cbbdb1a2b43ed8b
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-03-12 20:11:52 -0300

    Modificado os arquivos das Aulas de IF/ELSE

--- exercicios-web/02-controle/ifElseIf.js
+++ exercicios-web/02-controle/ifElseIf.js
@@ -4,21 +4,21 @@ Number.prototype.entre = function (inicio, fim) {
 
 const imprimirResultado = function (nota) {
     if (nota.entre(9, 10)) {
-        console.log('Quadro de Honra')
+        console.log('Quadro de Honra');
     } else if(nota.entre(7, 8.99)) {
-        console.log('Aprovado')
+        console.log('Aprovado');
     } else if(nota.entre(4, 6.99)) {
-        console.log('Recuperação')
+        console.log('Recuperação');
     } else if(nota.entre(0, 3.99)) {
-        console.log('Reprovado')
+        console.log('Reprovado');
     } else {
-        console.log('Nota inválida')
+        console.log('Nota inválida');
     }
 }
 
-imprimirResultado(10)
-imprimirResultado(8.9)
-imprimirResultado(6.55)
-imprimirResultado(2.3)
-imprimirResultado(-1)
-imprimirResultado(11)
+imprimirResultado(10);
+imprimirResultado(8.9);
+imprimirResultado(6.55);
+imprimirResultado(2.3);
+imprimirResultado(-1);
+imprimirResultado(11);
