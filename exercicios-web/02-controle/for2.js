348f462106faf9c0ac5058553187c5e350733d5b
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2022-03-13 00:18:31 -0300

    Modificado os arquivos acrescentando ';' nas sentenças dos códigos.

--- exercicios-web/02-controle/for2.js
+++ exercicios-web/02-controle/for2.js
@@ -1,7 +1,7 @@
 const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
 
 for (let i in notas) {
-    console.log(i, notas[i])
+    console.log(i, notas[i]);
 }
 
 const pessoa = {
@@ -12,5 +12,5 @@ const pessoa = {
 }
 
 for(let atributo in pessoa) {
-    console.log(`${atributo} = ${pessoa[atributo]}`)
+    console.log(`${atributo} = ${pessoa[atributo]}`);
 }
